5e62b22c8366593c0640729e0d3f76de0b142a03
Gleb Rysev <botkot1991@gmail.com>
2023-05-27 01:55:30 +0400

    add Example 8,9,10,11

---
+++ Example010_methodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 37, 13, 14, 51, 62, 37, 28 };
+
+int n = array.Length;
+int find = 37;
+int index = 0;
+
+while (index < n)
+{
+	if (array[index] == find)
+	{
+		Console.WriteLine(index);
+		break;
+	}
+	index++;
+}
